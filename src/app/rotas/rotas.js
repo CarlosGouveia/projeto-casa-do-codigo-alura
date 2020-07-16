f9d340e376371edce9269239b24368f8b7834de3
Carlos Gouveia <30901771+CarlosGouveia@users.noreply.github.com>
2020-07-16 09:14:49 -0300

    Utilizando sqlite3, padrão DAO e Promises

--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -1,7 +1,10 @@
+const db = require('../../config/database');
+const LivroDao = require('../infra/livro-dao');
+
 module.exports = (app) => {
-    app.get('/', function (req, resp) {
-        resp.send(
-            `
+  app.get('/', function (req, resp) {
+    resp.send(
+      `
             <html>
                 <head>
                     <meta charset="utf-8">
@@ -11,21 +14,21 @@ module.exports = (app) => {
                 </body> 
             </html>
           `
-        );
-    });
+    );
+  });
 
-    app.get('/livros', function (req, resp) {
-        resp.send(
-            `
-            <html>
-                <head>
-                    <meta charset="utf-8">
-                </head>
-                <body>
-                    <h1> Listagem de livros </h1>
-                </body> 
-            </html>
-          `
-        );
-    });
+  app.get('/livros', function (req, resp) {
+
+    const livroDao = new LivroDao(db);
+
+    livroDao.lista()
+      .then(livros => resp.marko(
+        require('../views/livros/lista/lista.marko'),
+        {
+          livros: livros
+        }
+      ))
+      .catch(erro => console.log('Ocorreu um erro.:', erro));
+
+  });
 }
